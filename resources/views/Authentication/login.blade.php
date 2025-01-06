475a238920b790b176620763c7718577c16c5fcd
thhs16 <thaehtapehtarsan@gmail.com>
2025-01-06 19:05:28 +0630

    POS

--- resources/views/Authentication/login.blade.php
+++ resources/views/Authentication/login.blade.php
@@ -21,32 +21,33 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                 </div>
-                                <form class="user">
+                                <form class="user" method="POST" action="{{ route('login') }}">
                                     <div class="form-group">
                                         <input type="email" class="form-control form-control-user"
                                             id="exampleInputEmail" aria-describedby="emailHelp"
-                                            placeholder="Enter Email Address...">
+                                            placeholder="Enter Email Address..." name="email" value="{{ old('email') }}">
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user"
-                                            id="exampleInputPassword" placeholder="Password">
+                                            id="exampleInputPassword" placeholder="Password" name="password" value="{{ old('password') }}>" >
                                     </div>
-                                    <div class="form-group">
+
+                                    {{-- <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck" name="">
                                             <label class="custom-control-label" for="customCheck">Remember
                                                 Me</label>
                                         </div>
-                                    </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
+                                    </div> --}}
+
+                                    <input type="submit" value="Login" class="btn btn-primary btn-user btn-block">
+
                                     <hr>
-                                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                                    <a href="{{ url('/auth/google/redirect') }}" class="btn btn-google btn-user btn-block">
                                         <i class="fab fa-google fa-fw"></i> Login with Google
                                     </a>
-                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    <a href="{{ url('/auth/github/redirect') }}" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Github
                                     </a>
                                 </form>
                                 <hr>
